17be945ca1a24f5216093dc8ea31f4f7797d27e9
Nonexentent <dbrooks26@mail.pima.edu>
2025-02-06 20:49:03 -0700

    Update cis129_lab02_shortStory.py
    This prints a real short story.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+#Arthur: Daniel Brooks
+#My first python lab in CIS 129
+#Prints a simple short story
+print("The cat jumped over a hat")
+print("The next day the cat jumped over the hat again")
+print("On the third day the cat did not find the hat")
+print("On the fourth day the cat jumped in the hat")
+print("The next day there was no cat, but just a hat a bit bigger than the day before")
